56d6db8e981525740ea496221721bff23aece993
Emekaer <Emekaer@users.noreply.github.com>
2016-04-26 00:26:42 +0100

    Delete Hello World.py

--- Hello World.py
+++
@@ -1,7 +0,0 @@
-print "Emeka Okafor" 
-print "Hello again"
-print "I like typing like this."
-print "This is fun."
-print 'Yay! Printing.'
-print "I'd much rather you 'not'."
-print ' I "said" do not touch this.'
